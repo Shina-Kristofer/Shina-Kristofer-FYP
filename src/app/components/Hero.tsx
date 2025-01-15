5c811ae7897adf6181464d4dd56327c8e1a1709d
Shina-Kristofer <amnamalik5599@gmail.com>
2025-01-15 06:10:15 +0500

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -19,8 +19,8 @@ export function Hero() {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Advanced Vehicle</span>{' '}
-                <span className="block text-blue-600 xl:inline">DetectionTechnology</span>
+                <span className="block xl:inline">Advanced Vehicle DetectionTechnology</span>{' '}
+                <span className="block text-blue-600 xl:inline"></span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Revolutionize your traffic management, parking systems, and security with our cutting-edge vehicle detection solution.
